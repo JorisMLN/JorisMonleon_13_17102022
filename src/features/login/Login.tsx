6ec445cdd277df237686159adbe5be9ba98cb9c3
Joris Monleon <monleon.joris@gmail.com>
2022-10-25 13:59:47 +0200

    Maj nav header

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,16 +1,23 @@
 import React, { LegacyRef, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from '../reducer/hook';
 import { storeLogin, storeToken} from '../reducer/reducers';
 
 const Login : React.FC= () => {
-  const count = useAppSelector(state => state.login);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
+  const userLogin = useAppSelector(state => state.login);
   const loginRef : LegacyRef<HTMLInputElement> | any | undefined = useRef();
-
+  
   const isStoringLogin = () => {
     console.log(loginRef.current.value);
     dispatch(storeLogin(loginRef.current.value))
+
+  }
+
+  const toBoard = () => {
+    navigate('/board');
   }
 
   // const isStoringToken = () => {
@@ -20,10 +27,11 @@ const Login : React.FC= () => {
   return (
     <>
       <div className='home'>
-        
+        <p>{userLogin}</p>
         <input ref={loginRef} type="text" />
         <button onClick={isStoringLogin}> Valider </button>
 
+        <button onClick={toBoard}> To Board </button>
       </div>
     </>
   )
--- src/layout/capsule/Header.tsx
+++ src/layout/capsule/Header.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Header : React.FC = () => {
+  const [isLogged, setIsLogged] = useState(false);
   const navigate = useNavigate();
 
   const toLoginFrame = () => {
@@ -11,14 +12,39 @@ const Header : React.FC = () => {
   const toHomeFrame = () => {
     navigate('/');
   }
+
+  const logOut = () => {
+    navigate('/');
+  }
+
+  useEffect(() => {
+    const actualPath = document.location.href;
+
+    if(actualPath === 'http://localhost:3000/board'){
+      setIsLogged(true);
+      console.log('true')
+    } else {
+      setIsLogged(false);
+      console.log('false')
+    }
+  })
  
   return (
     <>
+      {
+      isLogged === true ?
+      <div className='header'>
+        <button onClick={toHomeFrame}> Back Home </button>
+        <h1> Argent Bank </h1>
+        <button onClick={logOut}> logOut </button>
+      </div>
+      :
       <div className='header'>
         <button onClick={toHomeFrame}> Back Home </button>
         <h1> Argent Bank </h1>
         <button onClick={toLoginFrame}> Login ? </button>
       </div>
+      }
     </>
   )
 }
